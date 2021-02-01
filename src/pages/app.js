0520df053b181ab4b2705e944567015fd5069bb7
longuyen-dev <longuyen.dev@gmail.com>
2021-02-01 17:47:14 +0700

    fix

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { Router } from "@reach/router"
 import IndexPage from "./indexPage"
+import NotFoundPage from "./404"
 import Login from "../views/member/login"
 import Profile from "../views/member/profile"
 import PrivateRoute from "../components/privateRoute"
@@ -15,6 +16,7 @@ const App = () => (
         <IndexPage path="/" />
         <Login path="/member/login" />
         <PrivateRoute path="/member/profile" component={Profile} />
+        <NotFoundPage path="*" />
       </Router>
     </Layout>
   </Provider>
