1774c2a1a85876e2d71de5f73704313ad2b7de57
longuyen-dev <longuyen.dev@gmail.com>
2021-02-01 19:18:40 +0700

    add login page

--- src/views/member/login.js
+++ src/views/member/login.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react"
-
+import { Link } from "gatsby"
 import SEO from "../../components/seo"
-import { Form, Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import FloatingLabelInput from 'react-floating-label-input';
 import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { navigate } from "gatsby"
-
 import { login } from "../../redux/actions/auth"
 
 const Login = () => {
@@ -30,32 +30,46 @@ const Login = () => {
   return (
     <>
       <SEO title="Login" />
-      <h1>Login</h1>
-      {
-        errorMessage.length > 0 ? (
-          <div id="errorMsg">
-            <ul>
-              {errorMessage.map((err, i) => (
-                <li key={`err_${i}`}>{err}</li>
-              ))}
-            </ul>
-          </div>
-        ) : null
-      }
-
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-        </Form.Group>
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={e => handleLogin(e)}>
-          Submit
-        </Button>
-      </Form>
+      <Card style={{ background: 'none', border: 'none', marginTop: '30px' }}>
+        <Card.Body>
+          <h3 style={{ textAlign: 'center' }}>Sign in to your account</h3>
+          {
+            errorMessage.length > 0 ? (
+              <div id="errorMsg">
+                <ul>
+                  {errorMessage.map((err, i) => (
+                    <li key={`err_${i}`}>{err}</li>
+                  ))}
+                </ul>
+              </div>
+            ) : null
+          }
+          <p style={{ textAlign: 'center', fontSize: '14px' }}>Don't have an account? <Link to="/">Sign Up</Link></p>
+          <form>
+            <FloatingLabelInput
+              id="email"
+              label="Email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <br />
+            <FloatingLabelInput
+              type="password"
+              id="password"
+              label="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <br />
+            <input
+              type="submit"
+              className="custom-button-dark"
+              value="Log in"
+              onClick={e => handleLogin(e)} />
+          </form>
+          <Link to="/" style={{ fontSize: '14px', color: 'inherit' }}>Can't remember your password?</Link>
+        </Card.Body>
+      </Card>
     </>
   )
 }
